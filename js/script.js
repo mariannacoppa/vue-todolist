88d1f026c51ccff2287135ffce971082c91617f1
mariannacoppa <densimetra@gmail.com>
2024-07-20 11:45:43 +0200

    [UPD] created objects array

--- js/script.js
+++ js/script.js
@@ -2,7 +2,31 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            tasks: [
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'pulizie',
+                    done: true
+                },
+                {
+                    text: 'nuotare',
+                    done: true
+                },
+                {
+                    text: 'studiare',
+                    done: false
+                },
+                {
+                    text: 'leggere',
+                    done: false
+                }
+            ]
         }
+    },
+    methods: {
+
     }
 }).mount('#app');
